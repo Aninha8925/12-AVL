476a186325225cae61e47c3090f6681f6e9b0a5f
Analice da Mata <142676052+Aninha8925@users.noreply.github.com>
2026-05-26 12:07:57 -0300

    Fix rotation functions to return new root nodes
    Updated rotation functions to correctly return new root nodes after rotations.

--- AVL.cpp
+++ AVL.cpp
@@ -152,7 +152,8 @@ int fatorBalanceamento(NO* no) {
     return alturaNo(no->esq) - alturaNo(no->dir);
 }
 
-NO* girarDireita(NO* y) {  
+NO* girarDireita(NO* y) {     
+
    /* Rotação simples à direita  
              y                x  
             / \              / \  
@@ -161,14 +162,19 @@ NO* girarDireita(NO* y) {
         T1  T2               T2  T3  
    */  
 
-   // Passo 1: Armazene o filho esquerdo de 'y' em uma variável temporária 'x'.  
+   // Passo 1: Armazene o filho esquerdo de 'y' em uma variável temporária 'x'. i'
+    NO* x = y->esq;
    // Passo 2: Transfira a subárvore direita de 'x' para a subárvore esquerda de 'y'.  
+    y->esq = x->dir;
    // Passo 3: Atualize 'x' para ser o novo nó raiz da subárvore.  
+    x->dir = y;
    // Passo 4: Recalcule as alturas dos nós afetados.  
+    y->altura = maior(alturaNo(y->esq), alturaNo(y->dir)) + 1;
+    x->altura = maior(alturaNo(x->esq), alturaNo(x->dir)) + 1;
    // Passo 5: Retorne o novo nó raiz ('x').  
 
 	// provisoriamente retorna o ponteiro passado como parâmetro
-	return y; 
+	return x; 
 }  
 
 NO* girarEsquerda(NO* x) {  
@@ -181,9 +187,14 @@ NO* girarEsquerda(NO* x) {
    */  
 
    // Passo 1: Armazene o filho direito de 'x' em uma variável temporária 'y'.  
+    NO* y = x->dir;
    // Passo 2: Transfira a subárvore esquerda de 'y' para a subárvore direita de 'x'.  
+    x->esq = y->dir;
    // Passo 3: Atualize 'y' para ser o novo nó raiz da subárvore.  
+    y->esq = x;
    // Passo 4: Recalcule as alturas dos nós afetados.  
+    x->altura = maior(alturaNo(x->esq), alturaNo(x->dir)) + 1;
+    y->altura = maior(alturaNo(y->esq), alturaNo(y->dir)) + 1;
    // Passo 5: Retorne o novo nó raiz ('y').  
 
 
